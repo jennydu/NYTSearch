25646dce40d3c738e5f7834241c5ac9cd3285acd
Jennifer Du <jenniferdu@fb.com>
2016-06-24 15:24:49 -0700

    Added the sharing feature

--- app/src/main/java/com/codepath/nytimessearch/activities/ArticleActivity.java
+++ app/src/main/java/com/codepath/nytimessearch/activities/ArticleActivity.java
@@ -1,8 +1,13 @@
 package com.codepath.nytimessearch.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -33,6 +38,31 @@ public class ArticleActivity extends AppCompatActivity {
         webview.loadUrl(article.getWebUrl());
 
 
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Sharing the url of the loaded article
+
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.menu_article, menu);
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        // Fetch reference to the share action provider
+
+
+        ShareActionProvider miShare = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        // get reference to WebView
+        WebView wvArticle = (WebView) findViewById(R.id.wvArticle);
+        // pass in the URL currently being used by the WebView
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+
+        miShare.setShareIntent(shareIntent);
+        return super.onCreateOptionsMenu(menu);
 
     }
 
